3c0d476fd209fff1e214f969fa30ea144d61dd33
ContainerisedNomad <97117117+ContainerisedNomad@users.noreply.github.com>
2022-10-07 15:29:46 +0100

    Added code to display escape characters.

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -7,4 +7,10 @@ print("...rebooting output motors ")
 print("...rebooting hover engine ")
 print()
 print("System online. ")
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a black slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
 
