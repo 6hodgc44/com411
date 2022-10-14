68d407d68c947673d78a8b142af3108c78759823
ContainerisedNomad <97117117+ContainerisedNomad@users.noreply.github.com>
2022-10-14 14:59:44 +0100

    Added if_.py

---
+++ basics/decisions/simple decision/if_.py
@@ -0,0 +1,9 @@
+print("What type of book is this? ")
+genre = input()
+print()
+print()
+if genre == "adventure":
+    print("I like adventure books! ")
+else:
+    print()
+print("Finished reading book. ")
