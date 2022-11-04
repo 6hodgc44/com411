6fc33afc856cf24e985c2195fb0ee6c69abb2ad6
ContainerisedNomad <97117117+ContainerisedNomad@users.noreply.github.com>
2022-11-04 15:50:25 +0000

    fixed function

--- basics/lists/tuple_task_one.py
+++ basics/lists/tuple_task_one.py
@@ -1,5 +1,5 @@
 def likelihood():
     probability = (50, 38, 27, 99, 4)
-    return (f"The step with the least likelihood of falling is", probability[-1])
+    print (f"The step with the least likelihood of falling is {probability[-1]}")
 
 likelihood()
