3eda8388b07e786bda0d8bebc50acc7262dd8d5a
ContainerisedNomad <97117117+ContainerisedNomad@users.noreply.github.com>
2022-11-04 15:39:16 +0000

    created function_tuple

---
+++ basics/lists/function_tuple.py
@@ -0,0 +1,6 @@
+def likelihood():
+    likelihoods = (50, 38, 27, 99, 4)
+    print (f"The minimum likelihood of falling is {min(likelihoods)}%")
+    print (f"The maximum likelihood of falling is {max(likelihoods)}%")
+
+likelihood()
