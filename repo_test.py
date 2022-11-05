517c92fbc094d043438a9fce89ff3a35203ec88b
ContainerisedNomad <97117117+ContainerisedNomad@users.noreply.github.com>
2022-11-05 15:49:58 +0000

    Modified test.py

--- repo_test.py
+++ repo_test.py
@@ -1,1 +1,15 @@
-print("hello")
+def sandwich():
+    ingredients = ["Bread", "Cheese", "Mayo", "Butter"]
+    ingredients.append("Ham")
+    return ingredients
+
+def list_items():
+    ing_list = sandwich()
+    while len(ing_list) < 7:
+        for i in ing_list:
+            print(i)
+        ing_list.append(str(input("Anything to add?")))
+        for i in ing_list:
+            print(i)
+
+list_items()
