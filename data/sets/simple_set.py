13f9ac4ae175085317717fea576873f437fd9086
ContainerisedNomad <97117117+ContainerisedNomad@users.noreply.github.com>
2022-11-11 15:00:04 +0000

    Added File

---
+++ data/sets/simple_set.py
@@ -0,0 +1,11 @@
+def observed():
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observations
+
+
+def run():
+    obs = observed()
+    print(obs)
+
+
+run()
