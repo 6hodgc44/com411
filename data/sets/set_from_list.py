2ec40a7082be79a4384784ce3f9312ad0b634356
ContainerisedNomad <97117117+ContainerisedNomad@users.noreply.github.com>
2022-11-12 14:24:21 +0000

    Finished set_from_list.py

--- data/sets/set_from_list.py
+++ data/sets/set_from_list.py
@@ -2,7 +2,7 @@ def observed():
     observations = []
     ask = 0
     while ask <= 6:
-        print("Please enter an observation:\n")
+        print("Please enter an observation:")
         observations.append(str(input()))
         print()
         ask += 1
@@ -13,14 +13,13 @@ def run():
     print("Counting Observations...")
     list_observed = observed()
     list_set = set()
+
     for i in list_observed:
         tup = (i, list_observed.count(i))
         list_set.add(tup)
-    loop = 0
 
-# Need to finish. Needs to print out: There were x number of item y and x number of item b etc. Use a loop with print statements
+    for item in list_set:
+        print(f"{item[0]} was observed {item[1]} times")
 
-#    for x in list_set:
-#       print(f"There were {})
 
 run()
